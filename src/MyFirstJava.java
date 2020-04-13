3cfe75cdb444e54269d4334860cc0b71af349bc4
hchau98 <chauhenry02@gmail.com>
2020-04-13 15:35:26 -0400

    Second Commit

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -5,6 +5,6 @@ This my first Java program will print out a message. Hello World
 public class MyFirstJava {
     public static void main (String[] args)
     {
-        System.out.println("Hello world.");
+        System.out.println("Hello henry.");
     }
 }
